c66e46234650b34e0633d90de1bcc0da49fe817c
Prince Umunna <princevictor12@gmail.com>
2024-04-28 18:19:16 +0100

    User model included with Auth Facade and the Validator

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
 use App\Http\Controllers\Api\BaseController as BaseController;
 
 class RegisterController extends BaseController
