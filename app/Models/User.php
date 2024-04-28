ae786950d6640f6c8ad0a2b2201165f90e38eb40
Prince Umunna <princevictor12@gmail.com>
2024-04-28 16:26:50 +0100

    Revert "modify the User model to include firstname, lastname, email, role which will be fillable fields"
    This reverts commit aebf625c334a2c7a08b8a596078758c9e59c5c7a.

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'firstname',
-        'lastname',
+        'name',
         'email',
         'password',
-        'role',
     ];
 
     /**
