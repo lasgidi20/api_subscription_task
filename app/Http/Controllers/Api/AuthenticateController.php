9f329b2632b330dcdb4ada97169ef9e4738c8037
Prince Umunna <princevictor12@gmail.com>
2024-04-28 16:37:09 +0100

    An Api Authentications Controller responsible for handling Authentication request and response. The class has a Login method and a logout method

---
+++ app/Http/Controllers/Api/AuthenticateController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthenticateController extends Controller
+{
+    public function login(Request $request)
+    {
+      if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+          $user = Auth::user(); 
+          $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
+          $success['firstname'] =  $user->firstname;
+          $success['lastname'] =  $user->lastname;
+          $success['email'] =  $user->email;
+          $success['role'] =  $user->role;
+ 
+          return $this->sendResponse($success, 'User login successfully.');
+      } 
+      else{ 
+          return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+      } 
+    }
+
+    public function logout()
+    {
+      Auth::logout();
+      return response()->json(['message' => 'Logged Out'], 200);
+    }
+}
+
