01ba31ec2197edc74220d9347dce88a8902c7edf
Prince Umunna <princevictor12@gmail.com>
2024-04-28 16:19:25 +0100

    ammend users migration table with columns firstname, lastname, email and role. Role column has been set to an enum with an array option of selecting role as a student or teacher or Parent

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('role', [ 'student', 'teacher', 'parent'])->default('student');
             $table->timestamps();
         });
 
