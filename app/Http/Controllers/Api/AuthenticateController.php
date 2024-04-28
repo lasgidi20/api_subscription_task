25375e96cd7278304e4e0f1d5259b543f7e57f90
Prince Umunna <princevictor12@gmail.com>
2024-04-28 18:18:09 +0100

    included User Model and also included the Auth Facade

--- app/Http/Controllers/Api/AuthenticateController.php
+++ app/Http/Controllers/Api/AuthenticateController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Api\BaseController as BaseController;
 use Illuminate\Http\Request;
 
