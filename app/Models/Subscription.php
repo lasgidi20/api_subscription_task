28b7dff881e17fd77625c2967588eed659b3a5cc
Prince Umunna <princevictor12@gmail.com>
2024-05-02 19:02:11 +0100

    Subscription model that belongs to a user with fields that are fillable. The belongs to relationship is setup here with the user

---
+++ app/Models/Subscription.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Subscription extends Model
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'user_id',
+        'confirm',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
