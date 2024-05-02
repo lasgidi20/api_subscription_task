1d8dca367204829aecb4f2c2525b1d0ae1df69a6
Prince Umunna <princevictor12@gmail.com>
2024-05-02 18:27:50 +0100

    This middleware is responsible for filtering out IP addresses that are on the Block list

---
+++ app/Http/Middleware/BlockedIPChecker.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BlockedIPChecker
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $restrictedIps = ['102.129.158.0'];
+        if(in_array($request->ip(), $restrictedIps)){
+            App:abort(403, 'Request forbidden');
+        } else {
+            return $next($request);
+        }
+    }
+}
