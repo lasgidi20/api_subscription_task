6bdae14cbf08f19708f6662c69ad4138be83199a
Prince Umunna <princevictor12@gmail.com>
2024-05-02 20:43:49 +0100

    The file contains Updated methods changes in the Subscription interface that must be implemented

--- app/Contract/SubscriptionRepositoryInterface.php
+++ app/Contract/SubscriptionRepositoryInterface.php
@@ -7,8 +7,8 @@ use App\Models\Subscription;
 interface SubscriptionRepositoryInterface 
 {
     public function getAllSubscriptions();
-    public function getSubscriptionById(Subscription $subscription);
-    public function deleteSubscription(Subscription $subscription);
+    public function getSubscriptionById($subscriptionId);
+    public function deleteSubscription($subscriptionId);
     public function createSubscription(array $attributes);
-    public function updateSubscription(Subscription $subscription, array $attributes);
+    public function updateSubscription($subscriptionId, array $new_attributes);
 }
