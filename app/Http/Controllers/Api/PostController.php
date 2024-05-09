76d351859806a4ab22b36db05a044c49b3454482
Prince Umunna <princevictor12@gmail.com>
2024-05-09 13:33:40 +0100

    Post controller responsible for CRUD resource. it is integrated using a Repository design pattern

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,12 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Contract\PostRepositoryInterface;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use App\Http\Requests\PostRequest;
 use App\Http\Controllers\Api\BaseController as BaseController;
-use Validator;
+
 use App\Models\User;
 
 class PostController extends BaseController
@@ -19,27 +18,24 @@ class PostController extends BaseController
     {
     }
     
-    public function index(): JsonResponse 
+    public function index()
     {
-        return response()->json([
-            'data' => $this->post_repository->getAllPost()
-        ]);
+        $posts = $this->post_repository->getAllPost();
+        
+        return $this->sendResponse($posts, 'Posts fetched Successfully');
     }
 
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
         $attributes = [
             'title'=> $request->title,
-            'description'=> $request->description,
-            'user_id' => $request->user_id
+            'user_id'=> $request->user_id,
+            'description' => $request->description,
         ];
 
-        return response()->json(
-            [
-                'data' => $this->post_repository->createPost($attributes)
-            ],
-            Response::HTTP_CREATED
-        );
+        $post_data = $this->post_repository->createPost($attributes);
+        
+        return $this->sendResponse($post_data, 'Posts fetched Successfully');
     }
 }
 
