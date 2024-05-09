7658c4e26d50e74530d5abc9b80c0e35e7675553
Prince Umunna <princevictor12@gmail.com>
2024-05-09 13:41:05 +0100

    Gates Authorization setup defined here

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+
     public function register(): void
     {
-        //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Gate::define('isParent', function (User $user) {
+            return $user->role === 'teacher';
+        });
     }
 }
